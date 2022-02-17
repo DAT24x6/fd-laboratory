ad386a38ce0136837f7d8ce2d5e584d3989a37b0
DAT24x6 <daisuke19300@gmail.com>
2022-02-18 02:24:17 +0900

    updade : コード追加
    パドルの描画設定とキーボードからの入力検知を追加。

--- scripts/breakout.js
+++ scripts/breakout.js
@@ -5,19 +5,29 @@ window.addEventListener('load', function(){
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d")
 
-//ボールの描画と位置管理の関数
+//ボールの描画設定と位置管理の関数
 function drawBall(){
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "purple";
+    ctx.fillStyle = "#5c7894";
     ctx.fill();
     ctx.closePath();
 }
 
-//ボールと棒の移動と軌道消去関数
+//パドルの描画設定関数
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#4c8068";
+    ctx.fill();
+    ctx.closePath();
+}
+
+//ボールとパドルの移動、描画と軌道消去関数。（20msループ）
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     x += dx;
     y += dy;
     // (x += dx) は (x = x + dx) と同じ
@@ -35,14 +45,6 @@ function draw(){
     if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
         dx = -dx;
     }
-
-    //棒
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
-        paddleX += 7;
-    }
-    else if(leftPressed && paddleX > 0) {
-        paddleX -= 7;
-    }
 }
 
 //20ms毎に再描画
@@ -59,4 +61,16 @@ var dy = -2;
 //ボールの半径指定
 var ballRadius = 10;
 
+//パドルのサイズと初期位置定数
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+//入力情報の保存変数
+var rightPressed = false;
+var leftPressed = false;
+
+//入力検知
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 })
